6e417edf9d5672609f64c15b19f44622c20817bd
autohome <joshdadbin@gmail.com>
2023-05-17 13:30:55 -0700

    delete button now working

--- client/src/components/LyricCarousel/Card.jsx
+++ client/src/components/LyricCarousel/Card.jsx
@@ -1,48 +1,50 @@
 import Styles from "./Card.module.css";
 import React, { useState } from "react";
 import { useSpring, animated } from "react-spring";
-import Button from "./Button";
+// import Button from "./Button";
 import EditModal from "./Modal";
 import { useMutation } from "@apollo/client";
 import { REMOVE_LYRIC } from "../../utils/mutations";
+import Styles1 from "./Button.module.css";
 
 function Card({ text, id }) {
-  const [show, setShow] = useState(false);
-  const [removeLyric] = useMutation(REMOVE_LYRIC);
+    const [show, setShow] = useState(false);
+    const [removeLyric] = useMutation(REMOVE_LYRIC);
 
-  const props3 = useSpring({
-    transform: show ? "scale(1.03)" : "scale(1)",
-    boxShadow: show ? "0 20px 25px rgb(0 0 0 / 25%)" : "0 2px 10px rgb(0 0 0 / 8%)",
-  });
+    const props3 = useSpring({
+        transform: show ? "scale(1.03)" : "scale(1)",
+        boxShadow: show ? "0 20px 25px rgb(0 0 0 / 25%)" : "0 2px 10px rgb(0 0 0 / 8%)",
+    });
 
-  const handleDelete = async () => {
-    try {
-      await removeLyric({
-        variables: {
-          _id: id,
-        },
-      });
-    } catch (error) {
-      console.error("Error deleting lyric:", error);
-    }
-  };
+    const handleDelete = async () => {
+        console.log("clicked")
+        try {
+        await removeLyric({
+            variables: {
+            lyricId: id,
+            },
+        });
+        } catch (error) {
+        console.error("Error deleting lyric:", error);
+        }
+    };
 
-  return (
-    <animated.div
-      className={Styles.card}
-      style={props3}
-      onMouseEnter={() => setShow(true)}
-      onMouseLeave={() => setShow(false)}
-    >
-      <h2>Title</h2>
-      <p>{text}</p>
-      <div className={Styles.btnn}>
-        <Button text="Edit" onClick={() => setShow(true)} />
-        <Button text="Delete" onClick={handleDelete} />
-      </div>
-      {show && <EditModal lyrics={{ text, id }} />}
-    </animated.div>
-  );
+    return (
+        <animated.div
+            className={Styles.card}
+            style={props3}
+            onMouseEnter={() => setShow(true)}
+            onMouseLeave={() => setShow(false)}
+        >
+            <h2>Title</h2>
+            <p>{text}</p>
+            <div className={Styles.btnn}>
+                {/* <Button text="Edit" onClick={() => setShow(true)} /> */}
+                <button text="Delete" className={Styles1.btn} onClick={handleDelete}>Delete</button>
+            </div>
+            {show && <EditModal lyrics={{ text, id }} />}
+        </animated.div>
+    );
 }
 
 export default Card;
